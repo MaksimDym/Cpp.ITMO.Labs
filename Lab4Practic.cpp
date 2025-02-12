66071d1194f80751689b2ebbd343a5417c0f0dee
MaksimDym <fanmaxon@yandex.ru>
2025-02-12 15:18:12 +0300

    create lab4

---
+++ Lab4Practic.cpp
@@ -0,0 +1,85 @@
+﻿//#include <iostream>
+//#include <cmath>
+//#include<windows.h>
+//#include <ctime>
+//#include <vector>
+//#include <string>
+//
+//using namespace std;
+
+//void fum_value(double k, double x, double y) {
+//	x = x + k;
+//	y = y + k;
+//}
+//
+//void fum_ptr(double k, double* x, double* y)
+//{
+//	*x = *x + k;
+//	*y = *y + k;
+//}
+//
+//void fum_ref(double k, double& x, double& y)
+//{
+//	x = x + k;
+//	y = y + k;
+//}
+//
+//void print(double x, double y)
+//{
+//	cout << "x = " << x << "; y = " << y << endl;
+//}
+//
+//void swap(int*, int*);
+//void swap(int&, int&);
+//
+//void swap(int* x, int* y)
+//{
+//	int temp;
+//	temp = *x;
+//	*x = *y;
+//	*y = temp;
+//}
+//void swap(int& x, int& y)
+//{
+//	int temp;
+//	temp = x;
+//	x = y;
+//	y = temp;
+//}
+
+//int main(){
+
+	/*SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);*/
+
+	//lab1
+
+	//double k = 2.5;
+	//double xv = 10;
+	//double yv = 10;
+
+	//print(xv, yv);
+
+	//fum_value(k, xv, yv); // Передача в функцию обычного параметра 
+	//print(xv, yv);
+
+	//fum_ptr(k, &xv, &yv); // Передача в функцию параметра указателя 
+	//print(xv, yv);
+
+	//fum_ref(k, xv, yv); // Передача в функцию параметра ссылки 
+	//print(xv, yv);
+
+	//return 0;
+
+	//lab2
+	
+	/*int x = 5, y = 10;
+	cout << "Перед первым вызовом swap с указателями: x = " << x << ", y = " << y << endl;
+	swap(&x, &y);
+	cout << "После первого вызова swap с указателями: x = " << x << ", y = " << y << endl;
+	cout << "Перед вторым вызовом swap со ссылками: x = " << x << ", y = " << y << endl;
+	swap(x, y);
+	cout << "После второго вызова swap со ссылками: x = " << x << ", y = " << y << endl;
+	return 0;*/
+
+//}
